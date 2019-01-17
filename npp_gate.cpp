cf00d13ae47d48b5003340de6773967b66832e40
Adam Weizhe Lin <v-adlin@expedia.com>
2019-01-17 10:24:23 +0800

    continue

--- npp_gate.cpp
+++ npp_gate.cpp
@@ -14,7 +14,7 @@
  *
  * The Original Code is mozilla.org code.
  *
- * The Initial Developer of the Original Code is 
+ * The Initial Developer of the Original Code is
  * Netscape Communications Corporation.
  * Portions created by the Initial Developer are Copyright (C) 1998
  * the Initial Developer. All Rights Reserved.
@@ -22,7 +22,7 @@
  * Contributor(s):
  *
  * Alternatively, the contents of this file may be used under the terms of
- * either the GNU General Public License Version 2 or later (the "GPL"), or 
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
  * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
  * in which case the provisions of the GPL or the LGPL are applicable instead
  * of those above. If you wish to allow use of your version of this file only
@@ -35,24 +35,24 @@
  *
  * ***** END LICENSE BLOCK ***** */
 
-////////////////////////////////////////////////////////////
-//
-// Implementation of plugin entry points (NPP_*)
-// most are just empty stubs for this particular plugin 
-//
+ ////////////////////////////////////////////////////////////
+ //
+ // Implementation of plugin entry points (NPP_*)
+ // most are just empty stubs for this particular plugin 
+ //
 #include "plugin.h"
 
 char*
 NPP_GetMIMEDescription(void)
 {
-  return "application/mozilla-npruntime-scriptable-test";
+	return "application/mozilla-npruntime-scriptable-test";
 }
 
 
 
 NPError NPP_Initialize(void)
 {
-  return NPERR_NO_ERROR;
+	return NPERR_NO_ERROR;
 }
 
 void NPP_Shutdown(void)
@@ -63,82 +63,82 @@ void NPP_Shutdown(void)
 // will be associated with this newly created plugin instance and 
 // will do all the neccessary job
 NPError NPP_New(NPMIMEType pluginType,
-                NPP instance,
-                uint16_t mode,
-                int16_t argc,
-                char* argn[],
-                char* argv[],
-                NPSavedData* saved)
-{   
-  if(instance == NULL)
-    return NPERR_INVALID_INSTANCE_ERROR;
+	NPP instance,
+	uint16_t mode,
+	int16_t argc,
+	char* argn[],
+	char* argv[],
+	NPSavedData* saved)
+{
+	if (instance == NULL)
+		return NPERR_INVALID_INSTANCE_ERROR;
 
-  NPError rv = NPERR_NO_ERROR;
+	NPError rv = NPERR_NO_ERROR;
 
-  CPlugin * pPlugin = new CPlugin(instance);
-  if(pPlugin == NULL)
-    return NPERR_OUT_OF_MEMORY_ERROR;
+	CPlugin * pPlugin = new CPlugin(instance);
+	if (pPlugin == NULL)
+		return NPERR_OUT_OF_MEMORY_ERROR;
 
-  instance->pdata = (void *)pPlugin;
-  return rv;
+	instance->pdata = (void *)pPlugin;
+	return rv;
 }
 
 // here is the place to clean up and destroy the CPlugin object
-NPError NPP_Destroy (NPP instance, NPSavedData** save)
+NPError NPP_Destroy(NPP instance, NPSavedData** save)
 {
-  if(instance == NULL)
-    return NPERR_INVALID_INSTANCE_ERROR;
+	if (instance == NULL)
+		return NPERR_INVALID_INSTANCE_ERROR;
 
-  NPError rv = NPERR_NO_ERROR;
+	NPError rv = NPERR_NO_ERROR;
 
-  CPlugin * pPlugin = (CPlugin *)instance->pdata;
-  if(pPlugin != NULL) {
-    pPlugin->shut();
-    delete pPlugin;
-  }
-  return rv;
+	CPlugin * pPlugin = (CPlugin *)instance->pdata;
+	if (pPlugin != NULL) {
+		pPlugin->shut();
+		delete pPlugin;
+	}
+	return rv;
 }
 
 // during this call we know when the plugin window is ready or
 // is about to be destroyed so we can do some gui specific
 // initialization and shutdown
-NPError NPP_SetWindow (NPP instance, NPWindow* pNPWindow)
-{    
-  if(instance == NULL)
-    return NPERR_INVALID_INSTANCE_ERROR;
+NPError NPP_SetWindow(NPP instance, NPWindow* pNPWindow)
+{
+	if (instance == NULL)
+		return NPERR_INVALID_INSTANCE_ERROR;
 
-  NPError rv = NPERR_NO_ERROR;
+	NPError rv = NPERR_NO_ERROR;
 
-  if(pNPWindow == NULL)
-    return NPERR_GENERIC_ERROR;
+	if (pNPWindow == NULL)
+		return NPERR_GENERIC_ERROR;
 
-  CPlugin * pPlugin = (CPlugin *)instance->pdata;
+	CPlugin * pPlugin = (CPlugin *)instance->pdata;
 
-  if(pPlugin == NULL) 
-    return NPERR_GENERIC_ERROR;
+	if (pPlugin == NULL)
+		return NPERR_GENERIC_ERROR;
 
-  // window just created
-  if(!pPlugin->isInitialized() && (pNPWindow->window != NULL)) { 
-    if(!pPlugin->init(pNPWindow)) {
-      delete pPlugin;
-      pPlugin = NULL;
-      return NPERR_MODULE_LOAD_FAILED_ERROR;
-    }
-  }
+	// window just created
+	if (!pPlugin->isInitialized() && (pNPWindow->window != NULL)) {
+		if (!pPlugin->init(pNPWindow)) {
+			delete pPlugin;
+			pPlugin = NULL;
+			return NPERR_MODULE_LOAD_FAILED_ERROR;
+		}
+	}
 
-  // window goes away
-  if((pNPWindow->window == NULL) && pPlugin->isInitialized())
-    return NPERR_NO_ERROR;
+	// window goes away
+	if ((pNPWindow->window == NULL) && pPlugin->isInitialized())
+		return NPERR_NO_ERROR;
 
-  // window resized
-  if(pPlugin->isInitialized() && (pNPWindow->window != NULL))
-    return NPERR_NO_ERROR;
+	// window resized
+	if (pPlugin->isInitialized() && (pNPWindow->window != NULL))
+		return NPERR_NO_ERROR;
 
-  // this should not happen, nothing to do
-  if((pNPWindow->window == NULL) && !pPlugin->isInitialized())
-    return NPERR_NO_ERROR;
+	// this should not happen, nothing to do
+	if ((pNPWindow->window == NULL) && !pPlugin->isInitialized())
+		return NPERR_NO_ERROR;
 
-  return rv;
+	return rv;
 }
 
 // ==============================
@@ -152,124 +152,124 @@ NPError NPP_SetWindow (NPP instance, NPWindow* pNPWindow)
 // in the bin/components folder
 NPError	NPP_GetValue(NPP instance, NPPVariable variable, void *value)
 {
-  if(instance == NULL)
-    return NPERR_INVALID_INSTANCE_ERROR;
+	if (instance == NULL)
+		return NPERR_INVALID_INSTANCE_ERROR;
 
-  NPError rv = NPERR_NO_ERROR;
+	NPError rv = NPERR_NO_ERROR;
 
-  if(instance == NULL)
-    return NPERR_GENERIC_ERROR;
+	if (instance == NULL)
+		return NPERR_GENERIC_ERROR;
 
-  CPlugin * plugin = (CPlugin *)instance->pdata;
-  if(plugin == NULL)
-    return NPERR_GENERIC_ERROR;
+	CPlugin * plugin = (CPlugin *)instance->pdata;
+	if (plugin == NULL)
+		return NPERR_GENERIC_ERROR;
 
-  switch (variable) {
-  case NPPVpluginNameString:
-    *((char **)value) = "NPRuntimeTest";
-    break;
-  case NPPVpluginDescriptionString:
-    *((char **)value) = "NPRuntime scriptability API test plugin";
-    break;
-  case NPPVpluginScriptableNPObject:
-    *(NPObject **)value = plugin->GetScriptableObject();
-    break;
-  default:
-    rv = NPERR_GENERIC_ERROR;
-  }
+	switch (variable) {
+	case NPPVpluginNameString:
+		*((char **)value) = "NPRuntimeTest";
+		break;
+	case NPPVpluginDescriptionString:
+		*((char **)value) = "NPRuntime scriptability API test plugin";
+		break;
+	case NPPVpluginScriptableNPObject:
+		*(NPObject **)value = plugin->GetScriptableObject();
+		break;
+	default:
+		rv = NPERR_GENERIC_ERROR;
+	}
 
-  return rv;
+	return rv;
 }
 
 NPError NPP_NewStream(NPP instance,
-                      NPMIMEType type,
-                      NPStream* stream, 
-                      NPBool seekable,
-                      uint16_t* stype)
+	NPMIMEType type,
+	NPStream* stream,
+	NPBool seekable,
+	uint16_t* stype)
 {
-  if(instance == NULL)
-    return NPERR_INVALID_INSTANCE_ERROR;
+	if (instance == NULL)
+		return NPERR_INVALID_INSTANCE_ERROR;
 
-  NPError rv = NPERR_NO_ERROR;
-  return rv;
+	NPError rv = NPERR_NO_ERROR;
+	return rv;
 }
 
-int32_t NPP_WriteReady (NPP instance, NPStream *stream)
+int32_t NPP_WriteReady(NPP instance, NPStream *stream)
 {
-  if(instance == NULL)
-    return NPERR_INVALID_INSTANCE_ERROR;
+	if (instance == NULL)
+		return NPERR_INVALID_INSTANCE_ERROR;
 
-  int32_t rv = 0x0fffffff;
-  return rv;
+	int32_t rv = 0x0fffffff;
+	return rv;
 }
 
-int32_t NPP_Write (NPP instance, NPStream *stream, int32_t offset, int32_t len, void *buffer)
-{   
-  if(instance == NULL)
-    return NPERR_INVALID_INSTANCE_ERROR;
+int32_t NPP_Write(NPP instance, NPStream *stream, int32_t offset, int32_t len, void *buffer)
+{
+	if (instance == NULL)
+		return NPERR_INVALID_INSTANCE_ERROR;
 
-  int32_t rv = len;
-  return rv;
+	int32_t rv = len;
+	return rv;
 }
 
-NPError NPP_DestroyStream (NPP instance, NPStream *stream, NPError reason)
+NPError NPP_DestroyStream(NPP instance, NPStream *stream, NPError reason)
 {
-  if(instance == NULL)
-    return NPERR_INVALID_INSTANCE_ERROR;
+	if (instance == NULL)
+		return NPERR_INVALID_INSTANCE_ERROR;
 
-  NPError rv = NPERR_NO_ERROR;
-  return rv;
+	NPError rv = NPERR_NO_ERROR;
+	return rv;
 }
 
-void NPP_StreamAsFile (NPP instance, NPStream* stream, const char* fname)
+void NPP_StreamAsFile(NPP instance, NPStream* stream, const char* fname)
 {
-  if(instance == NULL)
-    return;
+	if (instance == NULL)
+		return;
 }
 
-void NPP_Print (NPP instance, NPPrint* printInfo)
+void NPP_Print(NPP instance, NPPrint* printInfo)
 {
-  if(instance == NULL)
-    return;
+	if (instance == NULL)
+		return;
 }
 
 void NPP_URLNotify(NPP instance, const char* url, NPReason reason, void* notifyData)
 {
-  if(instance == NULL)
-    return;
+	if (instance == NULL)
+		return;
 }
 
 NPError NPP_SetValue(NPP instance, NPNVariable variable, void *value)
 {
-  if(instance == NULL)
-    return NPERR_INVALID_INSTANCE_ERROR;
+	if (instance == NULL)
+		return NPERR_INVALID_INSTANCE_ERROR;
 
-  NPError rv = NPERR_NO_ERROR;
-  return rv;
+	NPError rv = NPERR_NO_ERROR;
+	return rv;
 }
 
 int16_t	NPP_HandleEvent(NPP instance, void* event)
 {
-  if(instance == NULL)
-    return 0;
+	if (instance == NULL)
+		return 0;
 
-  int16_t rv = 0;
-  CPlugin * pPlugin = (CPlugin *)instance->pdata;
-  if (pPlugin)
-    rv = pPlugin->handleEvent(event);
+	int16_t rv = 0;
+	CPlugin * pPlugin = (CPlugin *)instance->pdata;
+	if (pPlugin)
+		rv = pPlugin->handleEvent(event);
 
-  return rv;
+	return rv;
 }
 
 NPObject *NPP_GetScriptableInstance(NPP instance)
 {
-  if(!instance)
-    return 0;
+	if (!instance)
+		return 0;
 
-  NPObject *npobj = 0;
-  CPlugin * pPlugin = (CPlugin *)instance->pdata;
-  if (!pPlugin)
-    npobj = pPlugin->GetScriptableObject();
+	NPObject *npobj = 0;
+	CPlugin * pPlugin = (CPlugin *)instance->pdata;
+	if (!pPlugin)
+		npobj = pPlugin->GetScriptableObject();
 
-  return npobj;
+	return npobj;
 }
